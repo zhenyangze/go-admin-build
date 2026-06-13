409b7cb00647e44c7e63b49acd9000aac757a4d4
zhenyangze <zhenyangze@gmail.com>
2026-06-13 15:29:35 +0800

    fix: pagination Disabled field and icon display
    - Add Disabled field to paginationLink struct (fixes template panic)
    - Change NavigationItem.Icon type from string to template.HTML
    - Update layout.tmpl to render Icon directly instead of via safeHTML
    - Add Font Awesome CSS CDN to layout.tmpl

--- app.go
+++ app.go
@@ -1192,7 +1192,7 @@ type layoutData struct {
 
 type menuItemView struct {
 	Title    string
-	Icon     string
+	Icon     template.HTML
 	URL      string
 	Active   bool
 	Expanded bool
@@ -1313,10 +1313,11 @@ type gridActionView struct {
 }
 
 type paginationLink struct {
-	Label   string
-	URL     string
-	Active  bool
-	Current bool
+	Label    string
+	URL      string
+	Active   bool
+	Current  bool
+	Disabled bool
 }
 
 type formView struct {
